f335dfa6a8c661a2ec8605e05719a7428076a1b1
Brad <bradleymcmanus@hotmal.com>
2016-01-02 13:02:45 -0600

    hello world secretkeys

---
+++ TwitBot/TwitBot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TweetSharp;
+
+namespace TwitBot
+{
+    class Program
+    {
+        static string tweetText = "";
+
+
+        static void Main(string[] args)
+        {
+            PostTweet("Hello World");
+        }
+        static void PostTweet(string textToTweet)
+        {
+            var service = new TwitterService("ConsumerKey, "ConsumerSecret");
+            service.AuthenticateWith("AccessToken", "AccessTokenSecret");
+            SendTweetOptions tweet = new SendTweetOptions();
+            tweet.Status = textToTweet;
+
+            service.SendTweet(tweet);
+
+
+        }
+
+    }
+
+}
