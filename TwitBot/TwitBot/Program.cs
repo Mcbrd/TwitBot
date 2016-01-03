3b56bc6cb133432c46eb6fb68a44aa19fd8ad04d
Brad <bradleymcmanus@hotmal.com>
2016-01-03 15:43:54 -0600

    clean code

--- TwitBot/TwitBot/Program.cs
+++ TwitBot/TwitBot/Program.cs
@@ -12,62 +12,21 @@ namespace TwitBot
 {
     class Program
     {
-
-
         static void Main(string[] args)
-        {
-            //Console.WriteLine("Press ESC to stop");
-
-            //while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape))
-            //{
+        {          
                 
             while (true)
             {
-            var lines = File.ReadAllLines("liners.txt");
-            var rando = new Random();
-            int randomLineNumber = rando.Next(0, lines.Length - 1);
-            string line = lines[randomLineNumber];
 
-            PostTweet(line);
-            Thread.Sleep(60000);
+                var lines = File.ReadAllLines("liners.txt");
+                var rando = new Random();
+                int randomLineNumber = rando.Next(0, lines.Length - 1);
+                string line = lines[randomLineNumber];
+
+                PostTweet(line);
+                Thread.Sleep(60000);
             }
 
-            
-
-
-            //Console.WriteLine("Press any key to exit..."); 10 min
-
-            //Console.ReadKey();
-            
-
-
-
-            //Timer t = new Timer(60000); // 1 sec = 1000, 60 sec = 60000
-
-            //t.AutoReset = true;
-
-            //t.Elapsed += new System.Timers.ElapsedEventHandler(t_Elapsed);
-
-            //t.Start();
-
-
-            //wait
-            //string oneLiners;
-            //using (StreamReader reader = new StreamReader("liners.txt"))
-            //{
-            //    oneLiners = reader.ReadLine();
-            //}
-
-        }
-        //private static void t_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
-        //{
-           
-
-
-        //}
-
-        static void getText()
-        { 
         }
 
         static void PostTweet(string textToTweet)
@@ -84,14 +43,9 @@ namespace TwitBot
             string tokenSecretResult = string.Join("", tokenSecret);
             service.AuthenticateWith(tokenResut, tokenSecretResult);
 
-
             SendTweetOptions tweet = new SendTweetOptions();
             tweet.Status = textToTweet;
             service.SendTweet(tweet);
-
-
         }
-
     }
-
 }
